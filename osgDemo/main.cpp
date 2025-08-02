1900a35c68c65bc9fc3cf3ee6246c1ae354ad25e
Wei-Zz-z <2582663485@qq.com>
2025-08-02 13:16:19 +0800

    main.cpp

--- osgDemo/main.cpp
+++ osgDemo/main.cpp
@@ -6,9 +6,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
-    OsgToPng otp;
-    osg::Node* node = osgDB::readNodeFile("D:/baoli_OSGB_8CM/Production_3/Data/Tile_+000_+000/Tile_+000_+000_L19_00010t3.osgb");
-    otp.renderToPNG(node, "D:/isometric_view.png",2048,2048);
+    // OsgToPng otp;
+    // osg::Node* node = osgDB::readNodeFile("D:/baoli_OSGB_8CM/Production_3/Data/Tile_+000_+000/Tile_+000_+000_L19_00010t3.osgb");
+    // otp.renderToPNG(node, "D:/isometric_view.png",2048,2048);
     w.show();
     return a.exec();
 }
+
